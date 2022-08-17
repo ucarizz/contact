e5d80ba0bcdf3ac3feb558900a10d5ebc2b5c2a5
ucarizz <murat_ua@outlook.com>
2022-08-17 23:56:10 +0300

    initial

---
+++ src/components/Contacts/Form/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <input name="fullname" />
+        <input name="phoneNumber"/>
+    </div>
+  )
+}
+
+export default Form
